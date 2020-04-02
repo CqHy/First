a5e62376ec6f7cdbd0c2f4ede8c741a564c6f438
CqHy <894760290@qq.com>
2020-04-02 13:00:53 +0800

    11

--- notebooks/Users/yuhuang1@microsoft.com/hy_1.py
+++ notebooks/Users/yuhuang1@microsoft.com/hy_1.py
@@ -15,4 +15,40 @@ display(dbutils.fs.ls("dbfs:/databricks/scripts/postgresql-install.sh"))
 
 # COMMAND ----------
 
-123123
+script = """#!/bin/bash
+
+set -euxo pipefail
+
+if [[ $DB_IS_DRIVER = "TRUE" ]]; then
+  apt-get update
+  apt-get install -y gdebi-core
+  cd /tmp
+  # You can find new releases at https://rstudio.com/products/rstudio/download-server/debian-ubuntu/.
+  wget https://download2.rstudio.org/server/trusty/amd64/rstudio-server-1.2.5001-amd64.deb
+  sudo gdebi -n rstudio-server-1.2.5001-amd64.deb
+  rstudio-server restart || true
+fi
+"""
+
+dbutils.fs.mkdirs("/databricks/rstudio")
+dbutils.fs.put("/databricks/rstudio/rstudio-install.sh", script, True)
+
+# COMMAND ----------
+
+script = """#!/bin/bash
+set -euxo pipefail
+if [[ $DB_IS_DRIVER = "TRUE" ]]; then
+  apt-get update
+  apt-get install -y gdebi-core
+  cd /tmp
+  # You can find new releases at https://rstudio.com/products/rstudio/download-server/debian-ubuntu/.
+  wget https://download2.rstudio.org/server/trusty/amd64/rstudio-server-1.2.5033-amd64.deb
+ sudo gdebi -n rstudio-server-1.2.5033-amd64.deb
+  rstudio-server restart || true
+fi
+"""
+
+dbutils.fs.put("/databricks/rstudio/rstudio-install1.sh", script, True)
+
+# COMMAND ----------
+
